b2b7b764e655dddfcd8bbbe07b9b7df39600586c
Amy C <missch3n@gmail.com>
2019-09-11 08:11:20 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 first_number = 1
-second_number = 1
-third_number = 1 + 1
+second_number = 2
+third_number = 1 + 2
 fourth_number = first_number - second_number
 fifth_number = first_number * second_number
 sixth_number = first_number / second_number
